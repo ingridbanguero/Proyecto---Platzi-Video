5368ba2719496f6594d8822dc3efad309c9ba6e3
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-04-22 17:56:21 -0500

    se añade un evento visibility change que sirve para detener el video de la pagina Media Player cuando el usuario cambia dla pestaña

--- JS/plugins/AutoPause.js
+++ JS/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
     }
     run(player){
         this.player = player;
@@ -12,6 +13,8 @@ class AutoPause {
             threshold: this.threshold // Limite
         });
         observer.observe(this.player.media);
+
+        document.addEventListener('visibilitychange', this.handleVisibilityChange)
     }
 
     handleIntersection(entries){
@@ -23,6 +26,16 @@ class AutoPause {
        }else{
            this.player.pause();
        } 
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === "visible"
+
+        if(isVisible){
+            this.player.play();
+        }else{
+            this.player.pause();
+        }
     }
 }
 
