caf36f09a58c70934e92a933058e1a3d28bb47be
Ingrid Banguero <ingrid_banguero@hotmail.com>
2020-04-23 18:31:52 -0500

    Se creo el ServiceWorker v1 para que la aplicacion funcione offline

--- JS/mediaPlayer.js
+++ JS/mediaPlayer.js
@@ -2,7 +2,6 @@ import MediaPlayer from "./mediaPlayer2.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 import AutoPause from "./plugins/AutoPause.js";
 
-
 const video = document.querySelector("video") // El unico elemento con video es el que necesitamos
 const player = new MediaPlayer({
     el:video, 
@@ -20,3 +19,13 @@ buttonMute.onclick = () => {
       player.mute();
     }
   };
+console.log(navigator);
+//Detectar si el navegador del usuario da soporte a ServiceWorkers
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error)
+  });
+}else{
+  console.log('Tu navegador no soporta ServiceWorker')
+};
+  
---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = "v1";
+
+self.addEventListener('install', (e)=>{
+    e.waitUntil(precache()); // wait espera que la promesa se resuelva
+}) // Evento que se llama cuando se llama el SesrviceWorker
+
+self.addEventListener('fetch', (e)=>{
+    const request = e.request;
+    // Para que solamente acepte GET
+    if(request.method !== "GET"){
+        return; // Que no haga nada
+    }
+    e.respondWith(cachedResponse(request));
+
+    // Actualizar el cache
+    e.waitUntil(updateCache(request))
+})
+async function precache(){
+    const cache = await caches.open(VERSION);// Instancia de un cache (version 1)
+    return cache.addAll([ // Todos los archivos que hemos escrito
+        '/',
+        '/mediaplayer.html',
+        '/JS/mediaPlayer.js',
+        '/JS/mediaPlayer2.js',
+        '/JS/plugins/AutoPlay.js',
+        '/JS/plugins/AutoPause.js',
+        '/Style/mediaplayer.css',
+        '/Style/style.css',
+        '/Assets/BigBuckBunny.mp4'
+    ]);
+}
+
+async function cachedResponse(request){
+    const cache = await caches.open(VERSION);
+    const response = await cache.match(request);
+    return response || fetch(request);
+}
+
+async function updateCache(request){
+    const cache = await caches.open(VERSION);
+    const response = await fetch(request);
+    return cache.put(request, response).catch(error => {
+        console.log(error.message)
+    })
+}
